660cf1ceb2eb27c84da00b141fa6877f3e9663c0
Kachatha <kachatha4351@gmail.com>
2026-08-23 23:43:33 +0700

    Delete app directory

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main className="page">
-      <h1>เกี่ยวกับเรา</h1>
-      <p>ระบบ Student Grade จัดทำขึ้นเพื่ออำนวยความสะดวกในการตรวจสอบข้อมูลรายวิชา</p>
-    </main>
-  );
-}
